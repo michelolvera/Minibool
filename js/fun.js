91f771739349f0604f136a740ca6a8da383a75c7
Michel Olvera <michelolvera@icloud.com>
2018-03-23 02:02:14 -0600

    Registro y Login funcionando falta corregir comprobar correo y contraseña.

--- js/fun.js
+++ js/fun.js
@@ -3,28 +3,8 @@ var JsonIdioma;
 
 //El documento se ha cargado completamente, ahora se puede añadir funcionamiento.
 $(document).ready(function () {
-    CargarIdioma("es-MX")
-
+    CargarIdioma("es-MX");
 });
-
-function CargarFunciones() {
-    $("#login_form").submit(function (event) {
-        var usuario = $("#in_usuario").val();
-        var contraseña = $("#in_contrasena").val();
-        $.ajax({
-            method: "POST",
-            url: "php/selector.php",
-            data: { funcion: "login", userName: usuario, userPass: contraseña }
-        })
-            .done(function (msg) {
-                if (msg.value) {
-                    alert("Login Correcto");
-                } else {
-                    alert(JsonIdioma["loginFallido"]);
-                }
-            });
-    });
-}
 
 function CargarIdioma(idioma) {
     $.ajax({
@@ -39,6 +19,70 @@ function CargarIdioma(idioma) {
         .fail(function () {
             alert("Idioma no cargado, recargar pagina.");
         });
+}
+
+function CargarFunciones() {
+    $("#login_form").submit(function (event) {
+        var usuario = $("#in_usuario").val();
+        var contraseña = $("#in_contrasena").val();
+        $.ajax({
+            method: "POST",
+            url: "php/Selector.php",
+            data: { funcion: "login", userName: usuario, userPass: contraseña }
+        })
+            .done(function (msg) {
+                if (msg == 1) {
+                    location.href = "principal.html";
+                } else {
+                    alert(JsonIdioma["loginFallido"]);
+                }
+            });
+    });
+
+    $("#new_user_form").submit(function (event) {
+        var usuario = $("#in_usuario").val();
+        var nombre = $("#in_nombre").val();
+        var apellidoPaterno = $("#in_apellido_paterno").val();
+        var apellidoMaterno = $("#in_apellido_materno").val();
+        var correo = $("#in_correo").val();
+        var contraseña = $("#in_contrasena").val();
+        var pais = $("#combo_pais").val();
+        var estudiante = $('[name=in_estudia_si]:checked').val();
+        var escuela = $("#in_escuela").val();
+        var carrera = $("#in_carrera").val();
+        var semestre = $("#in_semestre").val();
+
+        $.ajax({
+            method: "POST",
+            url: "php/Selector.php",
+            data: {
+                funcion: "nuevoUsuario",
+                nombreUsuario: usuario,
+                nombreReal: nombre,
+                apellidoP: apellidoPaterno,
+                apellidoM: apellidoMaterno,
+                correoElectronico: correo,
+                passUsuario: contraseña,
+                codigoPais: pais,
+                esEstudiante: estudiante,
+                nombreEscuela: escuela,
+                nombreCarrera: carrera,
+                numSemestre: semestre
+            }
+        })
+            .done(function (msg) {
+                if (msg == 1) {
+                    alert(JsonIdioma["registroExitoso"]);
+                    location.href = "login.html";
+                }
+                else if(msg == 2){
+                    alert(JsonIdioma["registroExiste"]);
+                } 
+                else {
+                    alert(JsonIdioma["registroFallido"]);
+                }
+            });
+    });
 }
 
 function ComprobarRegex(regex, expresion) {
--- php/selector.php
+++ php/selector.php
@@ -9,7 +9,23 @@
             $userName = $_REQUEST["userName"];
             $userPass = $_REQUEST["userPass"];
             $respuesta = consultaSQL("CALL sp_validar_usuario('".$userName."','".$userPass."');");
-            echo $respuesta[0][0];
+            echo $respuesta[0]['Login'];
+        break;
+
+        case 'nuevoUsuario':
+            $nombreUsuario = $_REQUEST["nombreUsuario"];
+            $nombreReal = $_REQUEST["nombreReal"];
+            $apellidoP = $_REQUEST["apellidoP"];
+            $apellidoM = $_REQUEST["apellidoM"];
+            $correoElectronico = $_REQUEST["correoElectronico"];
+            $passUsuario = $_REQUEST["passUsuario"];
+            $codigoPais = $_REQUEST["codigoPais"];
+            $esEstudiante = $_REQUEST["esEstudiante"];
+            $nombreEscuela = $_REQUEST["nombreEscuela"];
+            $nombreCarrera = $_REQUEST["nombreCarrera"];
+            $numSemestre = $_REQUEST["numSemestre"];
+            $respuesta = consultaSQL("CALL sp_crear_usuario('".$nombreUsuario."','".$nombreReal."','".$apellidoP."','".$apellidoM."','".$correoElectronico."','".$codigoPais."','".$passUsuario."','".$esEstudiante."','".$nombreEscuela."','".$nombreCarrera."','".$numSemestre."','0');");
+            echo $respuesta[0]['Creado'];
         break;
     }
 ?>
