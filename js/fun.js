bc5a5a392ff8b965da9b569878d30f19fa821f8a
Michel Olvera <michelolvera@icloud.com>
2018-04-08 14:20:00 -0500

    Funcion para modificacion de cookies y fraces en frances

--- js/fun.js
+++ js/fun.js
@@ -3,7 +3,14 @@ var JsonIdioma;
 
 //El documento se ha cargado completamente, ahora se puede añadir funcionamiento.
 $(document).ready(function () {
-    CargarIdioma("es-MX");
+    var lang = getCookie("lang");
+    if(lang != ""){
+        CargarIdioma(lang);
+    }else{
+        CargarIdioma("es-MX");
+        lang = "es-MX"
+    }
+    setCookie("lang", lang, 30);
 });
 
 function CargarIdioma(idioma) {
@@ -17,7 +24,7 @@ function CargarIdioma(idioma) {
             CargarFunciones();
         })
         .fail(function () {
-            alert("Idioma no cargado, recargar pagina.");
+            alert("Error: Idioma no cargado, recargar pagina.");
         });
 }
 
@@ -95,4 +102,27 @@ function CargarFunciones() {
 
 function ComprobarRegex(regex, expresion) {
     return regex.test(expresion);
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
