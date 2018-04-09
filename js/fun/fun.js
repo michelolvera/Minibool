e3bc9baf71f6f4de749c3b0863580f9d6a10ec17
Michel Olvera <michelolvera@icloud.com>
2018-04-09 02:42:38 -0500

    Mejoras de ident

--- js/fun/fun.js
+++ js/fun/fun.js
@@ -3,61 +3,61 @@ var JsonIdioma;
 
 //El documento se ha cargado completamente, ahora se puede añadir funcionamiento.
 $(document).ready(function () {
-    //Validar Cookie de usuario
-    ValidarCookie();//Cada pagina maneja esto de manera diferente.
-    //Cargar cookie de Idioma
-    var lang = getCookie("lang");
-    if(lang != ""){
-        CargarIdioma(lang);
-    }else{
-        CargarIdioma("es-MX");
-        lang = "es-MX"
-    }
-    setCookie("lang", lang, 30);
-    CargarFunciones();
+  //Validar Cookie de usuario
+  ValidarCookie();//Cada pagina maneja esto de manera diferente.
+  //Cargar cookie de Idioma
+  var lang = getCookie("lang");
+  if(lang != ""){
+    CargarIdioma(lang);
+  }else{
+    CargarIdioma("es-MX");
+    lang = "es-MX"
+  }
+  setCookie("lang", lang, 30);
+  CargarFunciones();
 });
 
 function CargarIdioma(idioma) {
-    $.ajax({
-        method: "post",
-        url: langPath,
-        dataType: "json"
-    })
-        .done(function (jsonObject) {
-            JsonIdioma = jsonObject[idioma];
-            CargarTextosPagina();
-        })
-        .fail(function () {
-            alert("Error: Idioma no cargado, recargar pagina.");
-        });
+  $.ajax({
+    method: "post",
+    url: langPath,
+    dataType: "json"
+  })
+  .done(function (jsonObject) {
+    JsonIdioma = jsonObject[idioma];
+    CargarTextosPagina();
+  })
+  .fail(function () {
+    alert("Error: Idioma no cargado, recargar pagina.");
+  });
 }
 
 function ComprobarRegex(regex, expresion) {
-    return regex.test(expresion);
+  return regex.test(expresion);
 }
 
 function setCookie(cname, cvalue, exdays) {
-    if(exdays!=0){
-      var d = new Date();
-      d.setTime(d.getTime() + (exdays*24*60*60*1000));
-      var expires = "expires="+ d.toUTCString();
-      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    }else
-      document.cookie = cname + "=" + cvalue + ";path=/";
+  if(exdays!=0){
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }else
+  document.cookie = cname + "=" + cvalue + ";path=/";
 }
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
--- php/selector.php
+++ php/selector.php
@@ -1,31 +1,31 @@
 <?php
-    include 'ManejoSQL.php';
-    include 'Usuario.php';
+include 'ManejoSQL.php';
+include 'Usuario.php';
 
-    $funcion = $_REQUEST["funcion"];
+$funcion = $_REQUEST["funcion"];
 
-    switch ($funcion){
-        case "login":
-            $userName = $_REQUEST["userName"];
-            $userPass = $_REQUEST["userPass"];
-            $respuesta = consultaSQL("CALL sp_validar_usuario('".$userName."','".$userPass."');");
-            echo $respuesta[0]['Login'];
-        break;
+switch ($funcion){
+  case "login":
+  $userName = $_REQUEST["userName"];
+  $userPass = $_REQUEST["userPass"];
+  $respuesta = consultaSQL("CALL sp_validar_usuario('".$userName."','".$userPass."');");
+  echo $respuesta[0]['Login'];
+  break;
 
-        case 'nuevoUsuario':
-            $nombreUsuario = $_REQUEST["nombreUsuario"];
-            $nombreReal = $_REQUEST["nombreReal"];
-            $apellidoP = $_REQUEST["apellidoP"];
-            $apellidoM = $_REQUEST["apellidoM"];
-            $correoElectronico = $_REQUEST["correoElectronico"];
-            $passUsuario = $_REQUEST["passUsuario"];
-            $codigoPais = $_REQUEST["codigoPais"];
-            $esEstudiante = $_REQUEST["esEstudiante"];
-            $nombreEscuela = $_REQUEST["nombreEscuela"];
-            $nombreCarrera = $_REQUEST["nombreCarrera"];
-            $numSemestre = $_REQUEST["numSemestre"];
-            $respuesta = consultaSQL("CALL sp_crear_usuario('".$nombreUsuario."','".$nombreReal."','".$apellidoP."','".$apellidoM."','".$correoElectronico."','".$codigoPais."','".$passUsuario."','".$esEstudiante."','".$nombreEscuela."','".$nombreCarrera."','".$numSemestre."','0');");
-            echo $respuesta[0]['Creado'];
-        break;
-    }
+  case 'nuevoUsuario':
+  $nombreUsuario = $_REQUEST["nombreUsuario"];
+  $nombreReal = $_REQUEST["nombreReal"];
+  $apellidoP = $_REQUEST["apellidoP"];
+  $apellidoM = $_REQUEST["apellidoM"];
+  $correoElectronico = $_REQUEST["correoElectronico"];
+  $passUsuario = $_REQUEST["passUsuario"];
+  $codigoPais = $_REQUEST["codigoPais"];
+  $esEstudiante = $_REQUEST["esEstudiante"];
+  $nombreEscuela = $_REQUEST["nombreEscuela"];
+  $nombreCarrera = $_REQUEST["nombreCarrera"];
+  $numSemestre = $_REQUEST["numSemestre"];
+  $respuesta = consultaSQL("CALL sp_crear_usuario('".$nombreUsuario."','".$nombreReal."','".$apellidoP."','".$apellidoM."','".$correoElectronico."','".$codigoPais."','".$passUsuario."','".$esEstudiante."','".$nombreEscuela."','".$nombreCarrera."','".$numSemestre."','0');");
+  echo $respuesta[0]['Creado'];
+  break;
+}
 ?>
