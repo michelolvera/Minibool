b50b45adb041e794f63b9734d8955e4640d79087
Alan Cruz <alan@localhost.localdomain>
2018-04-09 11:21:41 -0500

    Administracion llenar tabla

---
+++ js/fun/Administracion.js
@@ -0,0 +1,38 @@
+var phpPath = "php/Selector.php";
+var langPath = "json/string.json";
+var tabla='<tr>'+
+'<th id="text_tabla_nombre">Nombre</th>'+
+'<th id="text_tabla_apellido_pat">Apellido paterno</th>'+
+'<th id="text_tabla_apellido_mat">Apellido materno</th>'+
+'<th id="text_tabla_accion">Accion</th> '+
+'</tr>';
+
+function CargarFunciones() {
+    llenar_tabla("","","");
+}
+
+function ValidarCookie() {
+
+}
+
+function CargarTextosPagina(){
+
+}
+
+function llenar_tabla(nombre, apellidoPat, apellidoMat) {
+    $.ajax({
+        method: "post",
+        url: phpPath,
+        data: {funcion:"consultaUsuarios", likeNombre:nombre, likeApellidoPat:apellidoPat, likeApellidoMat:apellidoMat},
+        dataType: "json"
+      })
+      .done(function (jsonObject) {
+        console.log(jsonObject);
+      })
+      .fail(function () {
+        alert("Error");
+      });
+    for(var ind=0; ind <datos.lengt; ind++){
+        d+='<tr>'
+    }
+}
