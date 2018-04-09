24d42516676f3888c2e4f7fd162fee1a1b6a40fc
Arreygue Avila Jorge Luis <arreygueavilatecmor.mx>
2018-04-09 01:21:59 -0500

    imagenes de logos en principal

--- js/visual.js
+++ js/visual.js
@@ -9,3 +9,10 @@ function closeNav() {
     $('#exampleModal').on('shown.bs.modal', function () {
         $('#exampleModal').trigger('focus')
     })
+
+function mFuncion(){
+    document.getElementById("intFuncion").style.display = block;
+}
+function oFuncion(){
+    document.getElementById("intFuncion").style.display = none;
+}
