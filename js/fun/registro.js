f42276ab534bd9080ddbc586eab0e66e3fc9007c
Michel Olvera <michelolvera@icloud.com>
2018-04-09 02:53:48 -0500

    Correccion de error al crear usuario.

--- js/fun/registro.js
+++ js/fun/registro.js
@@ -1,58 +1,59 @@
 var langPath = "../json/string.json";
 
 function CargarFunciones() {
-    $("#new_user_form").submit(function (event) {
-        var usuario = $("#in_usuario").val();
-        var nombre = $("#in_nombre").val();
-        var apellidoPaterno = $("#in_apellido_paterno").val();
-        var apellidoMaterno = $("#in_apellido_materno").val();
-        var correo = $("#in_correo").val();
-        var contraseña = $("#in_contrasena").val();
-        var pais = $("#combo_pais").val();
-        var estudiante = $('[name=in_estudia_si]:checked').val();
-        var escuela = $("#in_escuela").val();
-        var carrera = $("#in_carrera").val();
-        var semestre = $("#in_semestre").val();
+  $("#new_user_form").submit(function (event) {
+    var usuario = $("#in_usuario").val();
+    var nombre = $("#in_nombre").val();
+    var apellidoPaterno = $("#in_apellido_pat").val();
+    var apellidoMaterno = $("#in_apellido_mat").val();
+    var correo = $("#in_correo").val();
+    var contraseña = $("#in_contrasenia").val();
+    var pais = $("#combo_pais").val();
+    var estudiante = $('[name=in_estudia_si]:checked').val();
+    var escuela = $("#in_escuela").val();
+    var carrera = $("#in_carrera").val();
+    var semestre = $("#in_semestre").val();
 
-        $.ajax({
-            method: "POST",
-            url: "php/Selector.php",
-            data: {
-                funcion: "nuevoUsuario",
-                nombreUsuario: usuario,
-                nombreReal: nombre,
-                apellidoP: apellidoPaterno,
-                apellidoM: apellidoMaterno,
-                correoElectronico: correo,
-                passUsuario: contraseña,
-                codigoPais: pais,
-                esEstudiante: estudiante,
-                nombreEscuela: escuela,
-                nombreCarrera: carrera,
-                numSemestre: semestre
-            }
-        })
-            .done(function (msg) {
-                if (msg == 1) {
-                    alert(JsonIdioma["registroExitoso"]);
-                    location.href = "../";
-                }
-                else if(msg == 2){
-                    alert(JsonIdioma["registroExiste"]);
-                }
-                else {
-                    alert(JsonIdioma["registroFallido"]);
-                }
-            });
+    $.ajax({
+      method: "POST",
+      url: "../php/Selector.php",
+      data: {
+        funcion: "nuevoUsuario",
+        nombreUsuario: usuario,
+        nombreReal: nombre,
+        apellidoP: apellidoPaterno,
+        apellidoM: apellidoMaterno,
+        correoElectronico: correo,
+        passUsuario: contraseña,
+        codigoPais: pais,
+        esEstudiante: estudiante,
+        nombreEscuela: escuela,
+        nombreCarrera: carrera,
+        numSemestre: semestre
+      }
+    })
+    .done(function (msg) {
+      console.log(msg);
+      if (msg == 1) {
+        alert(JsonIdioma["registroExitoso"]);
+        location.href = "../";
+      }
+      else if(msg == 2){
+        alert(JsonIdioma["registroExiste"]);
+      }
+      else {
+        alert(JsonIdioma["registroFallido"]);
+      }
     });
+  });
 
-    $("input[type=radio][name=in_estudia_si]").change(function (event){
-        if(this.value == 1){
-            $("#PreguntasEstudiante").css("display","block");
-        }else{
-            $("#PreguntasEstudiante").css("display","none");
-        }
-    });
+  $("input[type=radio][name=in_estudia_si]").change(function (event){
+    if(this.value == 1){
+      $("#PreguntasEstudiante").css("display","block");
+    }else{
+      $("#PreguntasEstudiante").css("display","none");
+    }
+  });
 }
 
 function CargarTextosPagina(){
@@ -90,5 +91,5 @@ function CargarTextosPagina(){
 }
 
 function ValidarCookie() {
-//No necesario ya que se registrara.
+  //No necesario ya que se registrara.
 }
