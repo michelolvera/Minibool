4432f5ca87acdfbff82268780e1872a1d225349f
Michel Olvera <michelolvera@icloud.com>
2018-04-10 18:58:37 -0500

    Mejoras en el gestor de idiomas.

--- js/fun/login.js
+++ js/fun/login.js
@@ -1,7 +1,7 @@
 var langPath = "json/string.json";
 
 function CargarFunciones() {
-  $("#combo_lang").val(getCookie("lang"));
+  CargarListaIdiomas();
   $("#login_form").submit(function (event) {
     var usuario = $("#in_usuario").val();
     var contraseña = $("#in_contrasena").val();
@@ -10,45 +10,62 @@ function CargarFunciones() {
       url: "php/Selector.php",
       data: { funcion: "login", userName: usuario, userPass: contraseña }
     })
-    .done(function (msg) {
-      if (msg == 1) {
-        if($("#in_recordar").is(":checked")){
-          setCookie("user", usuario, 30);
-          setCookie("pass", contraseña, 30);
-        }else {
-          setCookie("user", usuario, 0);
-          setCookie("pass", contraseña, 0);
+      .done(function (msg) {
+        if (msg == 1) {
+          if ($("#in_recordar").is(":checked")) {
+            setCookie("user", usuario, 30);
+            setCookie("pass", contraseña, 30);
+          } else {
+            setCookie("user", usuario, 0);
+            setCookie("pass", contraseña, 0);
+          }
+          location.href = "principal/";
+        } else {
+          alert(JsonIdioma["loginFallido"]);
         }
-        location.href = "principal/";
-      } else {
-        alert(JsonIdioma["loginFallido"]);
-      }
-    });
+      });
   });
 
-  $("#combo_lang").change(function (event){
+  $("#combo_lang").change(function (event) {
     setCookie("lang", this.value, 30);
     CargarIdioma(this.value);
   });
 }
 
 function ValidarCookie() {
-  if(getCookie("user") != "" && getCookie("pass") != ""){
+  if (getCookie("user") != "" && getCookie("pass") != "") {
     location.href = "principal/";
   }
 }
 
-function CargarTextosPagina(){
+function CargarTextosPagina() {
   $("#titulo").text(JsonIdioma["tituloLogin"]);
   $("#titulo_h2").text(JsonIdioma["tituloLogin"]);
-  $("#lb_usuario").text(JsonIdioma["Usuario"]+":");
-  $("#in_usuario").attr("placeholder", JsonIdioma["Usuario"]+":");
-  $("#lb_contrasena").text(JsonIdioma["Pass"]+":");
-  $("#in_contrasena").attr("placeholder", JsonIdioma["Pass"]+":");
+  $("#lb_usuario").text(JsonIdioma["Usuario"] + ":");
+  $("#in_usuario").attr("placeholder", JsonIdioma["Usuario"] + ":");
+  $("#lb_contrasena").text(JsonIdioma["Pass"] + ":");
+  $("#in_contrasena").attr("placeholder", JsonIdioma["Pass"] + ":");
   $("#lb_recordar").text(JsonIdioma["RecordarPass"]);
   $("#btn_acceso").text(JsonIdioma["IniciarSesion"]);
   $("#btn_olvido_contrasena").text(JsonIdioma["OlvidastePass"]);
   $("#lb_no_cuenta").text(JsonIdioma["SinCuenta"]);
   $("#btn_crear_cuenta").text(JsonIdioma["CreaCuenta"]);
-  $("#lb_lang").text(JsonIdioma["Idioma"]+":");
+  $("#lb_lang").text(JsonIdioma["Idioma"] + ":");
+}
+
+function CargarListaIdiomas() {
+  $.ajax({
+    method: "post",
+    url: langPath,
+    dataType: "json"
+  })
+    .done(function (jsonObject) {
+      for (var lang in jsonObject) {
+        $("#combo_lang").append("<option value='" + lang + "'>" + jsonObject[lang]["nombreIdioma"] + "</option>");
+      }
+      $("#combo_lang").val(getCookie("lang"));
+    })
+    .fail(function () {
+      alert("Error: Idioma no cargado, recargar pagina.");
+    });
 }
