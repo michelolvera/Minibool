59c38f28a225064295d43476db7a3e398f203108
Alan Cruz <alan@localhost.localdomain>
2018-04-11 14:12:21 -0500

    Eliminar un usuario

--- js/fun/Administracion.js
+++ js/fun/Administracion.js
@@ -1,5 +1,7 @@
 var phpPath = "php/Selector.php";
 var langPath = "json/string.json";
+var idUsuarioEliminar=0;
+var queAccion="";
 var tabla='<tr>'+
 '<th id="text_tabla_nombre">Nombre</th>'+
 '<th id="text_tabla_apellido_pat">Apellido paterno</th>'+
@@ -34,10 +36,42 @@ function llenar_tabla(nombre, apellidoPat, apellidoMat) {
         $("#tabla").empty();
         $("#tabla").append(tabla);
         jsonObject.forEach(row => {
-          $("#tabla").append("<tr><td>"+row["nombre"]+"</td><td>"+row["apellido_paterno"]+"</td><td>"+row["apellido_materno"]+"</td><td><div class='btn-toolbar'><div class='btn-group'><button class='btn btn-default verde'>Detalles</button><button class='btn btn-default btn-danger'>Eliminar</button></div></div></td></tr>");
+          $("#tabla").append("<tr><td>"+row["nombre"]+"</td><td>"+row["apellido_paterno"]+"</td><td>"+row["apellido_materno"]+"</td><td><div class='btn-toolbar'><div class='btn-group'><button class='btn btn-default verde' data-toggle='modal'>Detalles</button><button href='#confirmacion' class='btn btn-default btn-danger' onclick='seleccion("+row["id_usuario"]+", \"eliminar\");' data-toggle='modal'>Eliminar</button></div></div></td></tr>");
         });
       })
       .fail(function () {
         alert("Error");
       });
+}
+
+function realizar_accion(){
+  switch(queAccion){
+    case 'eliminar':
+    $.ajax({
+      method: "post",
+      url: "php/Selector.php",
+      data: {funcion:"eliminarUsuario", idUsuario:idUsuarioEliminar, userName:getCookie("user"), userPass:getCookie("pass")},
+    })
+    .done(function (respuesta) {
+      if(respuesta == 1){
+        alert(JsonIdioma["EliminadoExistoso"]);
+      }else{
+        alert(JsonIdioma["EliminadoFallido"]);
+      }
+    })
+    .fail(function () {
+      alert("Error");
+    });
+    break;
+    case 'eliminarTodo':
+    alert("eliminar todo");
+    break;
+  }
+  $("#btn_cerrar").click();
+  llenar_tabla( $("#in_nombre").val(),$("#in_apellido_pat").val(),$("#in_apellido_mat").val());
+}
+function seleccion(usuario, accion){
+  idUsuarioEliminar=usuario;
+  queAccion=accion;
+  console.log("selecionados "+ idUsuarioEliminar +" "+ queAccion);
 }
--- php/Selector.php
+++ php/Selector.php
@@ -6,26 +6,26 @@ $funcion = $_REQUEST["funcion"];
 
 switch ($funcion){
   case "login":
-  $userName = $_REQUEST["userName"];
-  $userPass = $_REQUEST["userPass"];
-  $respuesta = consultaSQL("CALL sp_validar_usuario('".$userName."','".$userPass."');");
-  echo $respuesta[0]['Login'];
+    $userName = $_REQUEST["userName"];
+    $userPass = $_REQUEST["userPass"];
+    $respuesta = consultaSQL("CALL sp_validar_usuario('".$userName."','".$userPass."');");
+    echo $respuesta[0]['Login'];
   break;
 
   case 'nuevoUsuario':
-  $nombreUsuario = $_REQUEST["nombreUsuario"];
-  $nombreReal = $_REQUEST["nombreReal"];
-  $apellidoP = $_REQUEST["apellidoP"];
-  $apellidoM = $_REQUEST["apellidoM"];
-  $correoElectronico = $_REQUEST["correoElectronico"];
-  $passUsuario = $_REQUEST["passUsuario"];
-  $codigoPais = $_REQUEST["codigoPais"];
-  $esEstudiante = $_REQUEST["esEstudiante"];
-  $nombreEscuela = $_REQUEST["nombreEscuela"];
-  $nombreCarrera = $_REQUEST["nombreCarrera"];
-  $numSemestre = $_REQUEST["numSemestre"];
-  $respuesta = consultaSQL("CALL sp_crear_usuario('".$nombreUsuario."','".$nombreReal."','".$apellidoP."','".$apellidoM."','".$correoElectronico."','".$codigoPais."','".$passUsuario."','".$esEstudiante."','".$nombreEscuela."','".$nombreCarrera."','".$numSemestre."','0');");
-  echo $respuesta[0]['Creado'];
+    $nombreUsuario = $_REQUEST["nombreUsuario"];
+    $nombreReal = $_REQUEST["nombreReal"];
+    $apellidoP = $_REQUEST["apellidoP"];
+    $apellidoM = $_REQUEST["apellidoM"];
+    $correoElectronico = $_REQUEST["correoElectronico"];
+    $passUsuario = $_REQUEST["passUsuario"];
+    $codigoPais = $_REQUEST["codigoPais"];
+    $esEstudiante = $_REQUEST["esEstudiante"];
+    $nombreEscuela = $_REQUEST["nombreEscuela"];
+    $nombreCarrera = $_REQUEST["nombreCarrera"];
+    $numSemestre = $_REQUEST["numSemestre"];
+    $respuesta = consultaSQL("CALL sp_crear_usuario('".$nombreUsuario."','".$nombreReal."','".$apellidoP."','".$apellidoM."','".$correoElectronico."','".$codigoPais."','".$passUsuario."','".$esEstudiante."','".$nombreEscuela."','".$nombreCarrera."','".$numSemestre."','0');");
+    echo $respuesta[0]['Creado'];
   break;
 
   case 'consultaUsuarios':
@@ -34,6 +34,14 @@ switch ($funcion){
     $likeApellidoMat = $_REQUEST["likeApellidoMat"];
     $respuesta = consultaSQL("SELECT id_usuario, nombre, apellido_paterno, apellido_materno FROM usuarios WHERE nombre LIKE '%".$likeNombre."%' AND apellido_paterno LIKE '%".$likeApellidoPat."%' AND apellido_materno LIKE '%".$likeApellidoMat."%';");
     echo json_encode($respuesta);
+  break;
+
+  case 'eliminarUsuario':
+    $idUsuario = $_REQUEST["idUsuario"];
+    $userName = $_REQUEST["userName"];
+    $userPass = $_REQUEST["userPass"];
+    $respuesta = consultaSQL("CALL sp_eliminar_usuario('".$userName."','".$userPass."','".$idUsuario."');");
+    echo $respuesta[0]['Eliminado'];
   break;
 }
 ?>
