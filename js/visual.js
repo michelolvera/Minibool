2bc1e64f2ca46a6e86f56b2febc5690aabb30743
Arreygue Avila Jorge Luis <arreygueavilatecmor.mx>
2018-04-11 10:50:16 -0500

    pruebas desde atom git

--- js/visual.js
+++ js/visual.js
@@ -32,4 +32,8 @@ function oFuncion(){
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
-
+var crear = function(){
+  $('#content').html ='
+      hola
+  ';
+}
