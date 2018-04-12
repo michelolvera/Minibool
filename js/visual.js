7c3b4fa5d6fcef2c4e7055a8bbacb640f150b197
Arreygue Avila Jorge Luis <arreygueavilatecmor.mx>
2018-04-12 02:00:25 -0500

    tabla de verdad a medias

--- js/visual.js
+++ js/visual.js
@@ -34,37 +34,16 @@ var oFuncion = function(){
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
+
 var crear = function(){
   closeNav();
-  $('#bienvenidos').empty;
-  $('#content').html ='<table class="table table-striped">'+
-  '<thead>'+
-    '<tr>'+
-      '<th scope="col">#</th>'+
-      '<th scope="col">First</th>'+
-      '<th scope="col">Last</th>'+
-      '<th scope="col">Handle</th>'+
-    '</tr>'+
-  '</thead>'+
-  '<tbody>'+
-    '<tr>'+
-      '<th scope="row">1</th>'+
-      '<td>Mark</td>'+
-      '<td>Otto</td>'+
-      '<td>@mdo</td>'+
-    '</tr>'+
-    '<tr>'+
-      '<th scope="row">2</th>'+
-      '<td>Jacob</td>'+
-      '<td>Thornton</td>'+
-      '<td>@fat</td>'+
-    '</tr>'+
-    '<tr>'+
-      '<th scope="row">3</th>'+
-      '<td>Larry</td>'+
-      '<td>the Bird</td>'+
-      '<td>@twitter</td>'+
-    '</tr>'+
-  '</tbody>'+
-'</table>';
+  $('#content').load('tablaDeVerdad.html');
+  if($('#var4').is(':checked')){
+    console.log('cuatro');
+     $('td:nth-child(2),th:nth-child(2)').hide();
+  }
+  if($('#var5').is(':checked')){
+    $('td:nth-child(3),th:nth-child(3)').hide();
+  }
+
 };
