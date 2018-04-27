87eb541af2a7dfef92334944fed819c1e5756887
Arreygue Avila Jorge Luis <arreygueavilatecmor.mx>
2018-04-27 01:20:59 -0500

    validacion del primer grupo y de todos los parentesis balanceados

--- js/visual.js
+++ js/visual.js
@@ -38,6 +38,7 @@ var func = function () {
     $('#teclaD').addClass('btn-primary')
     $('#teclaE').removeClass('disabled')
   }
+  $('#btnIniciarConocido').hide();
 }
 var tabla = function () {
   $('#botonporTabladeVerdad').addClass('active');
@@ -46,7 +47,7 @@ var tabla = function () {
   $("#inpFuncion").hide();
   crear('#tablaVerdadMini', false);
   $('#inpTabla').show();
-
+  $('#btnIniciarConocido').show();
 }
 var mapak = function () {
   $('#botonporMapaK').addClass('active');
@@ -54,6 +55,7 @@ var mapak = function () {
   $('#botonporFuncion').removeClass('active');
   $("#inpFuncion").hide();
   $('#inpTabla').hide();
+  $('#btnIniciarConocido').show();
 }
 var limpiar = function(tabla) {
   $(tabla).html('');
@@ -108,7 +110,6 @@ var crear = function (tabla, isAl) {
     vueltas = 16;
   }
   if ($('#var5').is(':checked') || $('#Cvar5').is(':checked')) {
-    console.log("cinco jalas1");
     vueltas = 32;
   }
     tablaContent = '<thead>' +
@@ -121,7 +122,6 @@ var crear = function (tabla, isAl) {
     }
 
     if ($('#var5').is(':checked') || $('#Cvar5').is(':checked')) {
-      console.log("cinco primer if");
       tablaContent += '<th scope="col">D</th>';
       tablaContent += '<th scope="col">E</th>';
     }
@@ -202,7 +202,6 @@ var crear = function (tabla, isAl) {
     }
     tablaContent += '</tbody>';
     $(tabla).append(tablaContent);
-    console.log('fin append');
 
   };
 
@@ -245,35 +244,145 @@ var tabCircuitoclick = function() {
 
 function teclaAClick(){
   $('#inputFuncion').val($('#inputFuncion').val()+"A");
+  validarEntrada();
 }
 function teclaBClick(){
   $('#inputFuncion').val($('#inputFuncion').val()+"B");
+  validarEntrada();
 }
 function teclaCClick(){
   $('#inputFuncion').val($('#inputFuncion').val()+"C");
+  validarEntrada();
 }
 function teclaDClick(){
   if($('#teclaD').hasClass('btn-primary')) {
     $('#inputFuncion').val($('#inputFuncion').val()+"D");
+    validarEntrada();
   }
 }
 function teclaEClick(){
   if($('#teclaE').hasClass('btn-primary')) {
     $('#inputFuncion').val($('#inputFuncion').val()+"E");
+    validarEntrada();
   }
 }
 function teclaIzqClick(){
   $('#inputFuncion').val($('#inputFuncion').val()+"(");
+  validarEntrada();
 }
 function teclaDerClick(){
   $('#inputFuncion').val($('#inputFuncion').val()+")");
+  validarEntrada();
 }
 function teclaNegClick(){
   $('#inputFuncion').val($('#inputFuncion').val()+"'");
+  validarEntrada();
 }
 function teclaMasClick(){
   $('#inputFuncion').val($('#inputFuncion').val()+"+");
+  validarEntrada();
 }
 function teclaPorClick(){
   $('#inputFuncion').val($('#inputFuncion').val()+"*");
+  validarEntrada();
+}
+
+
+////////////////////  FUNCIONES PARA VALIDAR FUNCIONES  ///////////////////
+function validarEntrada(){
+  isBalanced();
+  if($("#inputFuncion").hasClass('is-invalid')){
+    return false;
+  }
+  var regNUM =/\([ABC\+']+\)/gi
+  console.log($( "#inputFuncion" ).val())
+  
+  if(regNUM.test($("#inputFuncion").val())){
+    $( "#inputFuncion" ).removeClass('is-invalid')
+    $( "#inputFuncion" ).addClass('is-valid')
+    $('#btnIniciarConocido').show();
+    return true;
+  }
+  else{
+    $( "#inputFuncion" ).removeClass('is-valid')
+    $( "#inputFuncion" ).addClass('is-invalid')
+    $('#btnIniciarConocido').hide();
+    return false;
+  }
+}
+
+var tokens = [ ['{','}'] , ['[',']'] , ['(',')'] ];
+// *** Check if character is an opening bracket ***
+function isOpenParenthesis(parenthesisChar) {
+  for (var j = 0; j < tokens.length; j++) {
+    if (tokens[j][0] === parenthesisChar) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// *** Check if opening bracket matches closing bracket ***
+function matches(topOfStack, closedParenthesis) {
+  for (var k = 0; k < tokens.length; k++) {
+    if (tokens[k][0] === topOfStack && tokens[k][1] === closedParenthesis) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// *** Checks if item is any sort of paranthesis ***
+function isParanthesis(char) {
+  var str = '{}[]()';
+  if (str.indexOf(char) > -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// *** Prints answer of the string to the HTML page  ***
+function printToScreen(bool) {
+  var parensStr = document.getElementById('inputFuncion');
+  var inputStr = parensStr.value
+
+  if (bool) {
+    $( "#inputFuncion" ).removeClass('is-invalid')
+    $( "#inputFuncion" ).addClass('is-valid')
+    $('#btnIniciarConocido').show();
+  } else {
+    $( "#inputFuncion" ).removeClass('is-valid')
+    $( "#inputFuncion" ).addClass('is-invalid')
+    $('#btnIniciarConocido').hide();
+  }
+}
+
+// *** We excute this function upon the event ***
+function isBalanced() {
+  var parensStr = document.getElementById('inputFuncion');
+  var inputStr = parensStr.value
+  if (inputStr === null) { printToScreen(true); }
+
+  var expression = inputStr.split('');
+  var stack = [];
+
+  for (var i = 0; i < expression.length; i++) {
+    if (isParanthesis(expression[i])) {
+      if (isOpenParenthesis(expression[i])) {
+        stack.push(expression[i]);
+      } else {
+        if (stack.length === 0) {
+          return printToScreen(false);
+        }
+        var top = stack.pop(); // pop off the top element from stack
+        if (!matches(top, expression[i])) {
+          return printToScreen(false);
+        }
+      }
+    }
+  }
+
+  var returnValue = stack.length === 0 ? true : false;
+  printToScreen(returnValue)
 }
